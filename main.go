e4f4c83245c780823aff6c5931494f1dcef89cab
Thomas Fischer <tdf.tomfischer@gmail.com>
2016-05-11 14:51:02 -0500

    Initial commit. Add emacs autosave files to .gitignore.

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/gragas/woobloo-frontend/config"
+	_ "github.com/lib/pq"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+var db *sql.DB
+
+func main() {
+	// connect to the database
+	// TODO: decide on a databse
+	// currently using: postgres
+	var err error
+	config := config.LoadConfig("config.json")
+	db, err = sql.Open(config.DBType, config.ConnectionString())
+	if err != nil {
+		panic(err)
+	}
+
+	// setup handlers
+	// http.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(config.ServerAddr, nil)
+}
+
+// Handlers
+
+// func indexHandler(w http.ResponseWriter, r *http.Request) {
+//	
+// }
